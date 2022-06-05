eb308747358bd71b0c2ff76fe0a174db219263dd
Aldanard <71127066+Aldanard@users.noreply.github.com>
2022-06-05 19:33:36 +0200

    Add files via upload

--- django/vojaci/urls.py
+++ django/vojaci/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+
 from . import views
+from django.urls import include, path
+from django.conf.urls.static import static
+from kostalek import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('vojaci/', views.VojaciListView.as_view(), name='soldiers'),
     path('vojaci/<int:pk>/', views.VojaciDetailView.as_view(), name='podrobnosti'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
